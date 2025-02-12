7c8ea7de1da436056f708bf02020ae0368002b0c
Mateus Almeida Silva <89556345+Mateeus10@users.noreply.github.com>
2025-02-12 08:52:46 -0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+
+const form = document.querySelector('#formulario')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const Inputpeso = e.target.querySelector('#peso')
+  const Inputaltura = e.target.querySelector('#altura')
+
+
+  const peso = Number(Inputpeso.value);
+  const altura = Number(Inputaltura.value)
+
+  if (!peso) {
+    setResultado('Peso Invalido', false)
+    return;
+  }
+
+  if (!altura) {
+    setResultado('Altura Invalido', false)
+    return;
+
+  }
+
+  const imc = getImc(peso, altura)
+  const nivelImc = getnivelImc(imc)
+
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+  setResultado(msg, true)
+
+});
+
+function getnivelImc(imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+}
+
+function criaP() {
+  const p = document.createElement('p');
+  return p;
+}
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector('#resultado');
+  resultado.innerHTML = '';
+
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add('paragrafo-resultado');
+  } else {
+    p.classList.add('bad');
+  }
+
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
